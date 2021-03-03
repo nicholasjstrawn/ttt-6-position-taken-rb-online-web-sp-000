a0ba7ff93d6b780029ebbad32029d21b3eb508a0
Nicholas Strawn <nicholasjstrawn@gmail.com>
2021-03-03 23:44:17 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,5 +2,7 @@
 def position_taken?(board, index)
   if board[index] == " " || board[index] == "" or board[index] == nil
     return false
+  elsif board[index] == "X" or board[index] == "O"
+    return true
   end
 end
